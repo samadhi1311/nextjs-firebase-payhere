f8c0c5b30c554a5cf42ba34d72460e447d3a2e88
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-09-15 12:53:02 +0530

    Updated header to match the changed dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
 			<main className='flex flex-col items-center row-start-2 gap-8 sm:items-start'>
-				<h1 className='text-5xl font-medium tracking-tighter text-transparent bg-gradient-to-b from-gray-100 to-gray-400 bg-clip-text'>Next.JS + Firebase + Stripe</h1>
+				<h1 className='text-5xl font-medium tracking-tighter text-transparent bg-gradient-to-b from-gray-100 to-gray-400 bg-clip-text'>Next.JS + Firebase + PayHere</h1>
 				<div className='flex flex-col items-center justify-center w-full'>
 					<ol className='flex flex-col gap-2 list-decimal'>
 						<li>
